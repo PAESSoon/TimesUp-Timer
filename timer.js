dc3da8b8babc5540537253c95f67f2cfb5bc4f7e
Pamela S-S <atheanae@gmail.com>
2022-03-16 15:00:42 -0400

    Timer - add control buttons

--- timer.js
+++ timer.js
@@ -1,22 +1,53 @@
 let row = document.getElementsByClassName("row");
-console.log(row);
+// console.log(row);
 let start = document.querySelector("#start");
 let pause = document.querySelector("#pause");
 
 let colorArr = ["#1e90ff", "#2ed573", "#ffa502", "#ff6348", "#ff4757"];
 
-function startTimer() {
-  start.setAttribute(
-    "style",
-    "box-shadow: inset 0 -4px 6px 4px #3f4ffffa, inset 0 1px 5px 17px #3f42fffa;"
-  );
+let timeOutSetUp;
 
-  for (let i = 0; i < row.length; i++) {
-    setTimeout(function () {
-      let backgroundColor = "background-color:" + colorArr[i] + ";";
-      row[i].setAttribute("style", backgroundColor);
-    }, i * 3000);
+// function startTimer() {
+//   start.setAttribute(
+//     "style",
+//     "box-shadow: inset 0 -4px 6px 4px #3f4ffffa, inset 0 1px 5px 17px #3f42fffa;"
+//   );
+
+//   for (let i = 0; i < row.length; i++) {
+//     timeOutSetUp = setTimeout(function () {
+//       console.log(i);
+//       let backgroundColor = "background-color:" + colorArr[i] + ";";
+//       row[i].setAttribute("style", backgroundColor);
+//     }, i * 3000);
+//   }
+// }
+
+let i = 0,
+  isActive = true;
+function startTimer() {
+  console.log(i);
+  if (!isActive) {
+    i = 0;
+    return;
   }
+  try {
+    let backgroundColor = "background-color:" + colorArr[i] + ";";
+    row[i].setAttribute("style", backgroundColor);
+    setTimeout(startTimer, 3000);
+  } catch (Exception) {
+    console.log(Exception);
+  }
+  i++;
 }
 
-function pauseTimer() {}
+function pauseTimer() {
+  isActive = false;
+}
+
+function resetTimer() {
+  isActive = true;
+  i = 0;
+  for (let j = 0; j < row.length; j++) {
+    row[j].setAttribute("style", "background-color:#f1f2f6;");
+  }
+}
